5440dd199a678da32f021851c2916837cbf0c7e3
Hide-KC <kcs.dev.labo@gmail.com>
2019-09-15 12:03:54 +0900

    generate javaDoc

--- helloworld/src/main/java/work/kcs_labo/helloworld/HelloWorldCore.kt
+++ helloworld/src/main/java/work/kcs_labo/helloworld/HelloWorldCore.kt
@@ -1,6 +1,13 @@
 package work.kcs_labo.helloworld
 
+/**
+ * Hello, World!
+ */
 class HelloWorldCore {
+
+  /**
+   * 引数に Hello を付けて返すだけ
+   */
   fun getHello(message: String) : String {
     return "Hello, $message!"
   }
